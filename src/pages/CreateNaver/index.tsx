4adb011c2fdfe362d9728e74ae5c3ff79cdbcc31
Gustavo <gcrodrigues413@gmail.com>
2020-08-25 20:24:42 -0300

    Create naver page development

---
+++ src/pages/CreateNaver/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Container from "../../components/Container";
+import FormNaver from "../../components/FormNaver";
+
+const CreateNaver: React.FC = () => {
+  return (
+    <Container>
+      <FormNaver />
+    </Container>
+  );
+};
+
+export default CreateNaver;
