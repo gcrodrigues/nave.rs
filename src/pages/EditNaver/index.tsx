91302a67b4dbc3a2752ba61ce66b130e4826cae4
Gustavo <gcrodrigues413@gmail.com>
2020-08-25 20:25:02 -0300

    Edit naver page development

---
+++ src/pages/EditNaver/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "../../components/Container";
+import FormNaver from "../../components/FormNaver";
+
+const EditNaver: React.FC = () => {
+  return (
+    <Container>
+      <FormNaver edit />
+    </Container>
+  );
+};
+
+export default EditNaver;
