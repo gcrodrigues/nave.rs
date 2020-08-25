fbea69a8dda32893272a7b118b4a761c127b8e17
Gustavo <gcrodrigues413@gmail.com>
2020-08-25 20:25:11 -0300

    Homepage development

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Container from "../../components/Container";
+import NaversList from "../../components/NaversList";
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <NaversList />
+    </Container>
+  );
+};
+
+export default Home;
